167820563d6488ad8fd80bd900dcac3f438b5017
Chris Chivers <chris.chivers@itv.com>
2020-01-11 20:32:27 +0000

    change to locating config

--- src/main/scala/io/chiv/flightscraper/SearchConfig.scala
+++ src/main/scala/io/chiv/flightscraper/SearchConfig.scala
@@ -1,27 +1,21 @@
 package io.chiv.flightscraper
 
-import java.io.File
-
-import cats.Monad
 import cats.effect.IO
-import io.chiv.flightscraper.model.Model._
 import io.chiv.flightscraper.model.Search
-import io.circe.Decoder
-import io.circe.jawn.JawnParser
-import io.circe.generic.semiauto._
+import io.circe.parser._
+
+import scala.io.Source
 
 object SearchConfig {
 
-  def load(file: File): IO[List[Search]] = {
-    new JawnParser()
-      .parseFile(file)
+  def load(str: String): IO[List[Search]] =
+    parse(str)
       .flatMap(_.as[List[Search]])
       .fold(
         err => IO.raiseError[List[Search]](err),
         searches => IO.pure(searches)
       )
-  }
 
   def load(): IO[List[Search]] =
-    load(new File(getClass.getResource("/search-config.json").getFile))
+    load(Source.fromResource("search-config.json").getLines().mkString)
 }
