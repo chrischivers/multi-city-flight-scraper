7090a764f914b9ce4fb7b74bb94bec060795c109
Chris Chivers <chris.chivers@itv.com>
2020-02-01 22:34:05 +0000

    setup dependencies included

--- src/main/scala/io/chiv/flightscraper/Main.scala
+++ src/main/scala/io/chiv/flightscraper/Main.scala
@@ -7,6 +7,9 @@ import io.chiv.flightscraper.kayak.KayakClient
 import io.chiv.flightscraper.selenium.WebDriver
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import sys.process._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
 
 object Main extends IOApp {
 
@@ -17,6 +20,7 @@ object Main extends IOApp {
 
     def app: IO[Unit] =
       for {
+        _      <- setupDependencies()
         config <- Config.load()
         searches <- SearchConfig
                      .load()
@@ -34,4 +38,14 @@ object Main extends IOApp {
     app.map(_ => ExitCode.Success)
 
   }
+
+  def setupDependencies(): IO[Unit] =
+    IO("wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz -O /tmp/geckodriver.tar.gz".!) >>
+      IO("tar xzf /tmp/geckodriver.tar.gz".!) >>
+      IO("mkdir /tmp/selenium".!) >>
+      IO("wget https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar -O /tmp/selenium.jar".!) >>
+      IO("wget http://www.java2s.com/Code/JarDownload/testng/testng-6.5.1.jar.zip -O /tmp/testng.jar".!) >>
+      IO("unzip /tmp/testng.jar.zip".!) >>
+      IO("screen -d -m -S selenium bash -c 'DISPLAY=:1 xvfb-run java -jar /tmp/selenium.jar'".!).void
+
 }
