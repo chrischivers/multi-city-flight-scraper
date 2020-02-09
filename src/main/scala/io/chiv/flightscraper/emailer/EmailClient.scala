14b2ea9ddaed1df34326d097d2237e6bc5367264
Chris Chivers <chris.chivers@itv.com>
2020-02-09 11:52:03 +0000

    emailer includes backoff retry

--- src/main/scala/io/chiv/flightscraper/emailer/EmailClient.scala
+++ src/main/scala/io/chiv/flightscraper/emailer/EmailClient.scala
@@ -1,7 +1,8 @@
 package io.chiv.flightscraper.emailer
 
 import cats.data.NonEmptyList
-import cats.effect.IO
+import cats.effect.{IO, Timer}
+import com.mailjet.client.errors.MailjetRateLimitException
 import com.mailjet.client.resource.Emailv31
 import com.mailjet.client.{ClientOptions, MailjetClient, MailjetRequest}
 import io.chiv.flightscraper.kayak.KayakParamsGrouping
@@ -10,6 +11,9 @@ import io.chiv.flightscraper.model.Search
 import io.chiv.flightscraper.selenium.WebDriver.Screenshot
 import io.chrisdavenport.log4cats.Logger
 import org.json.{JSONArray, JSONObject}
+import io.chiv.flightscraper.util._
+
+import scala.concurrent.duration._
 
 trait EmailClient {
   def sendNotification(search: Search, lowestPrices: NonEmptyList[(Price, KayakParamsGrouping)]): IO[Unit]
@@ -17,41 +21,41 @@ trait EmailClient {
 }
 
 object EmailClient {
-  def apply(accessKey: String, secretKey: String, emailAddress: String)(implicit logger: Logger[IO]) = new EmailClient {
+  def apply(accessKey: String, secretKey: String, emailAddress: String)(implicit logger: Logger[IO], timer: Timer[IO]) = new EmailClient {
 
-    private def send(subject: String, htmlBody: Option[String], attachmentArray: Option[JSONArray]) =
-      IO {
-        val client = new MailjetClient(accessKey, secretKey, new ClientOptions("v3.1"))
-        val request = new MailjetRequest(Emailv31.resource)
-          .property(
-            Emailv31.MESSAGES,
-            new JSONArray()
-              .put {
-                val jsonObject = new JSONObject()
-                  .put(Emailv31.Message.FROM,
-                       new JSONObject()
-                         .put("Email", emailAddress)
-                         .put("Name", "CC"))
-                  .put(Emailv31.Message.TO,
-                       new JSONArray()
-                         .put(
-                           new JSONObject()
-                             .put("Email", emailAddress)
-                             .put("Name", "CC")
-                         ))
-                  .put(Emailv31.Message.SUBJECT, subject)
-                  .put(Emailv31.Message.TEXTPART, "N/A")
+    private def send(subject: String, htmlBody: Option[String], attachmentArray: Option[JSONArray]) = {
+      val client = new MailjetClient(accessKey, secretKey, new ClientOptions("v3.1"))
+      val request = new MailjetRequest(Emailv31.resource)
+        .property(
+          Emailv31.MESSAGES,
+          new JSONArray()
+            .put {
+              val jsonObject = new JSONObject()
+                .put(Emailv31.Message.FROM,
+                     new JSONObject()
+                       .put("Email", emailAddress)
+                       .put("Name", "CC"))
+                .put(Emailv31.Message.TO,
+                     new JSONArray()
+                       .put(
+                         new JSONObject()
+                           .put("Email", emailAddress)
+                           .put("Name", "CC")
+                       ))
+                .put(Emailv31.Message.SUBJECT, subject)
+                .put(Emailv31.Message.TEXTPART, "N/A")
 
-                htmlBody.foreach(jsonObject.put(Emailv31.Message.HTMLPART, _))
-                attachmentArray.foreach(jsonObject.put(Emailv31.Message.ATTACHMENTS, _))
+              htmlBody.foreach(jsonObject.put(Emailv31.Message.HTMLPART, _))
+              attachmentArray.foreach(jsonObject.put(Emailv31.Message.ATTACHMENTS, _))
 
-                jsonObject
-              }
-          )
-        client.post(request)
-      }.flatMap { response =>
+              jsonObject
+            }
+        )
+      IO(client.post(request))
+    }.flatMap { response =>
         logger.info(s"Email sent. Response: ${response.getStatus}. Data: ${response.getData}")
       }
+      .withBackoffRetry(2.hours, 10)
 
     override def sendNotification(search: Search, lowestPrices: NonEmptyList[(Price, KayakParamsGrouping)]): IO[Unit] =
       send(
